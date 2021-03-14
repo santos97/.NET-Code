086d60ec3fcb0dd2cca43c3107128a92b1115a36
santos97 <santo.shet@gmail.com>
2021-03-14 18:43:11 +0530

    Simple user Login and Login with MVC app.

---
+++ trial2/trial2/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using trial2.Models;
+
+namespace trial2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            if (Session["UserID"] != null)
+            {
+                return RedirectToAction("UserDashBoard");
+
+            }
+            else
+            {
+                return View();
+
+            }
+            //return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserProfile objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (trial2Entities db = new trial2Entities())
+                {
+                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login");
+        }
+        //public ActionResult AddDetails()
+        //{
+
+        //}
+    }
+}
